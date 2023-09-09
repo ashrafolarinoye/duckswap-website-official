5a6c6273e720d63cb0caf5cf17d1be2b96400b41
Ashraf <104894013+ashrafolarinoye@users.noreply.github.com>
2023-09-09 08:40:15 +0000

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,3 @@
-import InfoSec from "@/components/InfoSec";
-import ScrollToTop from "@/components/ScrollToUp";
-
 export default function Home() {
   return (
     <main className="px-2 md:px-6 md:py-4 lg:px-32 lg:py-6">
