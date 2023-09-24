ae5196c571229297b53d7284d36d98d1b1cf30cd
Adefeyitimi Adeyeloja <adefeyitimi@gmail.com>
2023-09-24 14:35:52 +0100

    rename & fix

--- src/components/Duckonomics.tsx
+++ src/components/Duckonomics.tsx
@@ -16,10 +16,10 @@ export function Duckonomics() {
     ]
 
     return (
-        <section id="duckonomics" className="my-[10rem] px-6 space-y-5">
+        <section id="duckonomics" className="my-[10rem] px-6 space-y-5 py-5">
             <div className="text-center">
                 <h3 className=" font-extrabold text-white tracking-tight text-5xl">Ducko<span className="text-[#FDB833]">nomics</span></h3>
-                <p className="text-2xl text-white mx-auto w-8/12">The DUCKCOIN  is the native token of Duckchain. It serves as a governance token with a maximum supply and represents platform governance functions. This documentation provides an overview of the Duck token and its various use cases and functionalities within the Duckcoin ecosystem.</p>
+                <p className="text-2xl text-white mx-auto sm:w-8/12 w-full">The DUCKCOIN  is the native token of Duckchain. It serves as a governance token with a maximum supply and represents platform governance functions. This documentation provides an overview of the Duck token and its various use cases and functionalities within the Duckcoin ecosystem.</p>
             </div>
             <div className="mx-auto w-5/6 space-y-2">
                 <h3 className="text-[#FDB833] text-xl font-semibold">Token Emission
@@ -46,7 +46,7 @@ export function Duckonomics() {
             <div className="mx-auto w-5/6 space-y-2">
                 <h3 className="text-[#FDB833] text-xl font-semibold">Allocations
                 </h3>
-                <div className="text-white flex flex-nowrap sm:flex-wrap flex-col h-[10vh]">
+                <div className="text-white flex flex-nowrap sm:flex-wrap flex-col sm:h-[10vh] h-fit">
                     {allocations.map((text, index) => {
                         return (
                             <p key={index}>
