2e84fed4bcd80b61e9ac809be18aabc03a27b7e6
Adefeyitimi Adeyeloja <adefeyitimi@gmail.com>
2023-09-24 20:48:58 +0100

    update: update content

--- src/components/Duckmap.tsx
+++ src/components/Duckmap.tsx
@@ -7,14 +7,20 @@ export default function Duckmap() {
             <div className="flex flex-col justify-center items-center text-center text-white space-y-3">
                 <div>
                     <h5 className="text-white text-xl font-bold"><span className="font-semibold text-[#FDB833]">Phase</span> 1</h5>
-                    <p>Marketing, Gempade, Gamefi, Poolz and Gate exchange IDO,
-                        Duckcoin listing, and launching</p>
-                    <p><span className="text-[#FDB833]">2 weeks after IDO</span>: Blockchain, Duckswap and Bridge Launching</p>
+                    <p>Marketing, IEO/IDO,  Promotions,
+                        Duckcoin listing ( IDO and major CEX listing), and launching</p>
+                    <p><span className="text-[#FDB833]">(Stage 2) 2 weeks after IDO</span>: Blockchain, Duckswap and Bridge Launching</p>
+                    <p className="sm:w-6/12 w-full mx-auto"><span className="text-[#FDB833]">(Stage 3)</span>: Wallet launching, Synoptic Finance launch (A multi-infrastructure; composable omnichain native assets bridge on layerzero, axelar and Wanchain. Synoptic finance, enables users move assets across chains utilizing either axelar, layerzero or Wanchain infrastructure.)</p>
                 </div>
                 <div>
                     <h5 className="text-white text-xl font-bold"><span className="font-semibold text-[#FDB833]">Phase</span> 2</h5>
-                    <p>Hackathon, Dapps and projects onboarding.</p>
+                    <p>Hackathon, Dapps and projects onboarding, and Duckchain ecosystem online and Physical Events </p>
                 </div>
+                <div>
+                    <h5 className="text-white text-xl font-bold"><span className="font-semibold text-[#FDB833]">Phase</span> 3</h5>
+                    <p>Duckchain Ecosystem Roadmap V2</p>
+                </div>
+
                 <div>
                     <p>More Coming Soon...</p>
                 </div>
